fd71757cfb79afc5b5fc132d6ab5d8dc54308418
M <m@cognusion.com>
2018-07-22 10:08:58 -0400

    Initial commit of Task and its tests

---
+++ task.go
@@ -0,0 +1,145 @@
+package cronzilla
+
+import (
+	gerrors "github.com/go-errors/errors"
+
+	"context"
+	"errors"
+	"fmt"
+	"time"
+)
+
+// ErrTaskPanicError is an error returned if a Task panics during Run
+type ErrTaskPanicError struct {
+	message string
+}
+
+// Error returns the string message
+func (e ErrTaskPanicError) Error() string {
+	return e.message
+}
+
+// TaskFunc is a func that has no parameters and returns only error
+type TaskFunc func() error
+
+// ErrorlessTaskFunc wraps a func() into a TaskFunc
+// TODO: Fix Name
+func ErrorlessTaskFunc(f func()) TaskFunc {
+	return func() error {
+		f()
+		return nil
+	}
+}
+
+// Task is our... task
+type Task struct {
+	// Todo is a TaskFunc that gets called Every
+	Todo TaskFunc
+	// Every is a Duration for how often Todo()
+	Every time.Duration
+	// At is a Time to run Todo()
+	At time.Time
+	// crashed is tracks if Run has exited
+	crashed bool
+}
+
+// Run takes a context and error chan.
+// If Every is non-zero, calls Todo() Every until the context expires or is cancelled.
+// If At is non-zero, calls Todo() At.
+// If both Every and At are defined, will do both.
+// The error chan WILL BE CLOSED when the function exits, which is a good way to know that the task isn't running
+// anymore, otherwise will only pass non-nil errors from Todo(). If the error is an ErrTaskPanicError, then Todo() panic'd,
+// and the stack trace is returned on errorChan just before it is closed.
+func (t *Task) Run(ctx context.Context, errorChan chan error) {
+	t.run(ctx, errorChan, false)
+}
+
+// RunOnce takes an error chan, and calls Todo() once after an Every or At.
+// The error chan WILL BE CLOSED when the function exits, which is a good way to know that the task isn't running
+// anymore, otherwise will only pass non-nil errors from Todo(). If the error is an ErrTaskPanicError, then Todo() panic'd,
+// and the stack trace is returned on errorChan just before it is closed.
+func (t *Task) RunOnce(errorChan chan error) {
+	t.run(context.Background(), errorChan, true)
+}
+
+// run actually does the run
+func (t *Task) run(ctx context.Context, errorChan chan error, once bool) {
+	defer func() {
+		t.crashed = true
+		close(errorChan)
+	}()
+	defer func() {
+		if r := recover(); r != nil {
+			var err error
+			switch rt := r.(type) {
+			case string:
+				err = errors.New(rt)
+			case error:
+				err = rt
+			default:
+				err = errors.New(fmt.Sprintf("Unknown error: '%+v'", r))
+			}
+
+			// Don't block if tere isn't a reader
+			select {
+			case errorChan <- ErrTaskPanicError{gerrors.Wrap(err, 2).ErrorStack()}:
+			default:
+			}
+		}
+	}()
+	t.crashed = false
+
+	if t.At.Unix() > 0 {
+		atChan := make(chan struct{})
+		atTimer := time.AfterFunc(time.Until(t.At), func() {
+			defer func() { close(atChan) }()
+			if e := t.Todo(); e != nil {
+				// Don't block if tere isn't a reader
+				select {
+				case errorChan <- e:
+				default:
+				}
+			}
+		})
+
+		if t.Every == 0 {
+			// We won't have a ticker keeping us alive
+			select {
+			case <-ctx.Done():
+				return
+			case <-atChan:
+				// Win.
+				return
+			}
+		}
+		defer atTimer.Stop()
+	}
+
+	if t.Every > 0 {
+		ticker := time.NewTicker(t.Every)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ticker.C:
+				if e := t.Todo(); e != nil {
+					// Don't block if tere isn't a reader
+					select {
+					case errorChan <- e:
+					default:
+					}
+				}
+			}
+
+			if once {
+				return
+			}
+		}
+	}
+}
+
+// IsCrashed returns an internal state bool that is set if Run() was called, but has exited
+func (t *Task) IsCrashed() bool {
+	return t.crashed
+}
---
+++ task_test.go
@@ -0,0 +1,322 @@
+package cronzilla
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func Test_TaskRunEvery(t *testing.T) {
+
+	pingChan := make(chan int, 10)
+	errorChan := make(chan error, 1)
+	tFunc := func() error {
+		pingChan <- 1
+		return nil
+	}
+	task := Task{
+		Every: time.Millisecond,
+		Todo:  tFunc,
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Millisecond)
+	go task.Run(ctx, errorChan)
+
+	counter := 0
+OUT:
+	for {
+		select {
+		case p := <-pingChan:
+			counter += p
+		case e := <-errorChan:
+			if e != nil {
+				t.Errorf("Unexpected error %s\n", e)
+			} else {
+				break OUT
+			}
+		}
+	}
+
+	if !task.IsCrashed() {
+		t.Error("Expected IsCrashed true\n")
+	}
+	if counter != 5 {
+		t.Errorf("Expected 5, got %d\n", counter)
+	}
+}
+
+func Test_TaskRunAtEvery(t *testing.T) {
+
+	pingChan := make(chan int, 10)
+	errorChan := make(chan error, 1)
+	tFunc := func() error {
+		pingChan <- 1
+		return nil
+	}
+	task := Task{
+		Every: time.Millisecond,
+		At:    time.Now().Add(3 * time.Millisecond),
+		Todo:  tFunc,
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Millisecond)
+	go task.Run(ctx, errorChan)
+
+	counter := 0
+OUT:
+	for {
+		select {
+		case p := <-pingChan:
+			counter += p
+		case e := <-errorChan:
+			if e != nil {
+				t.Errorf("Unexpected error %s\n", e)
+			} else {
+				break OUT
+			}
+		}
+	}
+
+	if !task.IsCrashed() {
+		t.Error("Expected IsCrashed true\n")
+	}
+	if counter != 6 {
+		t.Errorf("Expected 6, got %d\n", counter)
+	}
+}
+
+func Test_TaskRunAt(t *testing.T) {
+
+	pingChan := make(chan int, 10)
+	errorChan := make(chan error, 1)
+	tFunc := func() error {
+		pingChan <- 1
+		return nil
+	}
+	task := Task{
+		Todo: tFunc,
+		At:   time.Now().Add(3 * time.Millisecond),
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Millisecond)
+	go task.Run(ctx, errorChan)
+
+	counter := 0
+OUT:
+	for {
+		select {
+		case p := <-pingChan:
+			counter += p
+		case e := <-errorChan:
+			if e != nil {
+				t.Errorf("Unexpected error %s\n", e)
+			} else {
+				break OUT
+			}
+		}
+	}
+
+	if !task.IsCrashed() {
+		t.Error("Expected IsCrashed true\n")
+	}
+	if counter != 1 {
+		t.Errorf("Expected 1, got %d\n", counter)
+	}
+}
+
+func Test_TaskRunAtError(t *testing.T) {
+
+	errorChan := make(chan error, 1)
+	tFunc := func() error {
+		return fmt.Errorf("Error")
+	}
+	task := Task{
+		Todo: tFunc,
+		At:   time.Now().Add(3 * time.Millisecond),
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Millisecond)
+	go task.Run(ctx, errorChan)
+
+	counter := 0
+OUT:
+	for {
+		select {
+		case e := <-errorChan:
+			if e != nil {
+				counter++
+			} else {
+				break OUT
+			}
+		}
+	}
+
+	if !task.IsCrashed() {
+		t.Error("Expected IsCrashed true\n")
+	}
+	if counter != 1 {
+		t.Errorf("Expected 1, got %d\n", counter)
+	}
+}
+
+func Test_TaskError(t *testing.T) {
+
+	errorChan := make(chan error, 1)
+	tFunc := func() error {
+		return fmt.Errorf("Error!")
+	}
+	task := Task{
+		Every: time.Millisecond,
+		Todo:  tFunc,
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Millisecond)
+	go task.Run(ctx, errorChan)
+
+	counter := 0
+OUT:
+	for {
+		select {
+		case e := <-errorChan:
+			if e != nil {
+				counter++
+			} else {
+				break OUT
+			}
+		}
+	}
+
+	if !task.IsCrashed() {
+		t.Error("Expected IsCrashed true\n")
+	}
+	if counter < 4 || counter > 6 {
+		t.Errorf("Expected error count 4..6, got %d\n", counter)
+	}
+}
+
+func Test_TaskRunPanic(t *testing.T) {
+
+	pingChan := make(chan int, 10)
+	errorChan := make(chan error, 1)
+	p := false
+	tFunc := func() error {
+		if !p {
+			p = true
+			panic("OMG")
+		}
+		pingChan <- 1
+		return nil
+	}
+	task := Task{
+		Every: time.Millisecond,
+		Todo:  tFunc,
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Millisecond)
+	go task.Run(ctx, errorChan)
+
+	counter := 0
+OUT:
+	for {
+		select {
+		case p := <-pingChan:
+			counter += p
+		case e := <-errorChan:
+			if e != nil {
+				switch et := e.(type) {
+				case ErrTaskPanicError:
+					// Win
+					_ = et.Error()
+					continue
+				default:
+					t.Errorf("Unexpected error %s %s\n", e, et)
+				}
+			} else {
+				break OUT
+			}
+		}
+	}
+
+	if !task.IsCrashed() {
+		t.Error("Expected IsCrashed true\n")
+	}
+	if counter > 0 {
+		t.Errorf("Expected 0, got %d\n", counter)
+	}
+}
+
+func Test_TaskRunOnce(t *testing.T) {
+
+	pingChan := make(chan int, 10)
+	errorChan := make(chan error, 1)
+	tFunc := func() error {
+		pingChan <- 1
+		return nil
+	}
+	task := Task{
+		Every: time.Millisecond,
+		Todo:  tFunc,
+	}
+
+	go task.RunOnce(errorChan)
+
+	counter := 0
+OUT:
+	for {
+		select {
+		case p := <-pingChan:
+			counter += p
+		case e := <-errorChan:
+			if e != nil {
+				t.Errorf("Unexpected error %s\n", e)
+			} else {
+				break OUT
+			}
+		}
+	}
+
+	if !task.IsCrashed() {
+		t.Error("Expected IsCrashed true\n")
+	}
+	if counter != 1 {
+		t.Errorf("Expected 1, got %d\n", counter)
+	}
+}
+
+func Test_TaskRunOnceErrorless(t *testing.T) {
+
+	pingChan := make(chan int, 10)
+	errorChan := make(chan error, 1)
+	tFunc := func() {
+		pingChan <- 1
+	}
+	task := Task{
+		Every: time.Millisecond,
+		Todo:  ErrorlessTaskFunc(tFunc),
+	}
+
+	go task.RunOnce(errorChan)
+
+	counter := 0
+OUT:
+	for {
+		select {
+		case p := <-pingChan:
+			counter += p
+		case e := <-errorChan:
+			if e != nil {
+				t.Errorf("Unexpected error %s\n", e)
+			} else {
+				break OUT
+			}
+		}
+	}
+
+	if !task.IsCrashed() {
+		t.Error("Expected IsCrashed true\n")
+	}
+	if counter != 1 {
+		t.Errorf("Expected 1, got %d\n", counter)
+	}
+}
